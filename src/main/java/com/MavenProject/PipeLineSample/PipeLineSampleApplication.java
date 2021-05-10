f357fd1f84fc7e8b3e198cf3aa885d3bb2ad96a5
student <vijaychowdarygutta@gmail.com>
2021-05-10 03:36:47 +0000

    PipeLine Sample

---
+++ src/main/java/com/MavenProject/PipeLineSample/PipeLineSampleApplication.java
@@ -0,0 +1,13 @@
+package com.MavenProject.PipeLineSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeLineSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeLineSampleApplication.class, args);
+	}
+
+}
